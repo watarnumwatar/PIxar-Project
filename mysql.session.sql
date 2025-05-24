1b547d29e674f247f30b160cdf47279e4e507d8d
thanarat <thanarat1.vy@gmail.com>
2025-05-24 15:17:26 +0700

    update report

--- mysql.session.sql
+++ mysql.session.sql
@@ -1,2 +1,14 @@
 SELECT 
-        FLOOR(EXTRACT(YEAR from release date) / 5) * 5 AS decade,
+    f.film,
+    EXTRACT(YEAR FROM f.release_date) AS release_year,
+    a.award_type,
+    a.status
+FROM
+    pixar_films f
+LEFT JOIN
+    aca a
+    ON f.film = a.film
+ORDER BY
+    release_year ;
+
+    
